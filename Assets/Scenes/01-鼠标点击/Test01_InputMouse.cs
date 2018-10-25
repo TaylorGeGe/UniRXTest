d5824729a3c3b928b80f0eb53a9934677b171f63
TaylorGeGe <991959229@qq.com>
2018-10-25 22:49:51 +0800

    第一版 两个demo 鼠标点击 UNIRX的实现，和 计时器 update 和协程和uniRx 计时器的实现

---
+++ Assets/Scenes/01-鼠标点击/Test01_InputMouse.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class Test01_InputMouse : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Observable.EveryUpdate()
+            .Where(_=> isDown()) 
+            .First()
+            .Subscribe(_=> DebugLog()); 
+
+         
+    }
+
+    public Action action;
+    Action<string> loaded;
+    public void hahah(string st)
+    { 
+        Debug.Log(st);
+    }
+    public void hahah2(Action<string> loaded )
+    {
+      
+        Debug.Log("哈哈哈");
+        loaded("??");
+    } 
+    public static void test(Action<string> loaded)
+    {
+
+        //loaded("我是我啊");
+
+       // Debug.Log("那我是谁");
+
+    }
+    public bool isDown()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+    public void DebugLog()
+    {
+        Debug.Log("Mouse left Down");
+    }
+}
---
+++ Assets/Scenes/02-计时器/CommonTimerExample.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class CommonTimerExample : MonoBehaviour {
+    private float mStartTime;
+    void Start() {
+        // 方法1  传统实现方法
+        mStartTime = Time.time;
+
+        //方法2 协程方式实现
+        StartCoroutine(Timer(2, () => { Debug.Log(" 协程  do sm" ); }));
+
+        //方法3 uniRx方式实现
+        Observable.Timer(TimeSpan.FromSeconds(5.0f)).Subscribe(_ => { Debug.Log(" uniRx do sm"); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 方法1  传统实现方法
+        if (Time.time-mStartTime>5)
+        {
+            //
+            Debug.Log("传统  do sm");
+
+            mStartTime = float.MaxValue;
+        }
+	}
+    
+   IEnumerator Timer(float seconds,Action  action)
+    {
+        yield return new WaitForSeconds(seconds);
+        action(); 
+    }
+}
